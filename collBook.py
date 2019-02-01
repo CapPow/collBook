cc08962654c86d4a6116e93ecbe1282379c9d238
Caleb Powell <CalebAdamPowell@gmail.com>
2019-02-01 10:25:32 -0500

    added legal preamble to collBook.py

--- collBook.py
+++ collBook.py
@@ -1,15 +1,30 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+#    This program is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU General Public License
+#    as published by the Free Software Foundation; either version 3
+#    of the License, or (at your option) any later version.
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 """
-.... (as of yet unnamed) is the desktop companion to (as of yet unnamed). 
-This project seeks to provide direct-to-database format field notes for 
-field researchers making biological collections. 
+
+collDesk is the desktop companion to collNote. Combined they are seek to
+provide a field-to-database solution designed for field biologists to gather
+and format “born digital” field notes into database ready formats.
+
 """
 __author__ = "Caleb Powell, Jacob Motley"
 __credits__ = ["Caleb Powell, Jacob Motley, Joey Shaw"]
 __email__ = "calebadampowell@gmail.com"
 __status__ = "Alpha"
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 
 import sys
